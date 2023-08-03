6824a2735c28ed794b97ab90b5b7cd1f86fa44e3
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2023-08-03 20:25:45 +0200

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pespinos <pespinos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 18:08:35 by pespinos          #+#    #+#             */
+/*   Updated: 2023/08/03 20:23:46 by pespinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int ft_strlen(char *str)
+{
+    int len;
+
+    len = 0;
+    while (str[len])
+        len++;
+    return (len);
+}
+
+int ft_number_pipes(char *str)
+{
+    int n_pipes;
+    int n;
+
+    n_pipes = 0;
+    n = 0;
+    while (str[n])
+    {
+        if (str[n] == '|')
+            n_pipes++;
+        n++;
+    }
+    return (n_pipes);
+}
+// HAY QUE REPASAR DESDE AQUI -----------------------------
+void    ft_str_copy(char *str_origin, char *str_destiny)
+{
+    int n;
+
+    n = 0;
+    while (str_origin[n])
+    {
+        str_destiny[n] = str_origin[n];
+        n++;
+    }
+}
+
+int ft_check_chars(char letter, char *str)
+{
+    int found;
+    int n;
+
+    found = 0;
+    n = 0;
+    while (str[n] && found == 0)
+    {
+        if (str[n] == letter)
+            found = 1;
+        n++;
+    }
+    return (found);
+}
+
+int ft_number_characters(char *str)
+{
+    int n;
+    int n_chars;
+
+    n = 0;
+    n_chars = 0;
+    while (str[n])
+    {
+        if (ft_check_chars(str[n + 1], "<>|") && str[n] != ' ')
+            n_chars++;
+        else if (ft_check_chars(str[n], "<>|") && str[n + 1] != ' ')
+            n_chars++;
+        n_chars++;
+        n++;
+    }
+    return (n_chars);
+}
+
+// char    *ft_separator(char *str)
+// {
+//     char    *result;
+// }
+
+//------------------------------------ HASTA AQUI
+
+int main()
+{
+    char str[] = "hola|que";
+    printf("NUMERO DE CARACTERES FUTUROS -> %i\n", ft_number_characters(str));
+    return (0);
+}
